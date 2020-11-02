cb368c53b79545bf4037bbf78a57ace80d1fc8e7
l07p <l07p@gmx.de>
2020-11-02 23:03:38 +0100

    product listed with wkn and name

--- database/investment/scripts/python/product.py
+++ database/investment/scripts/python/product.py
@@ -5,24 +5,34 @@ import psycopg2
 import psycopg2.extras
 import sys
 
-try:
-    conn = psycopg2.connect("dbname='investment' user='pi' host='192.168.178.54' password='ueber500mal'")
-    print('connected to database')
+class Product:
+    def __init__(self):
+        pass
 
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM products ORDER BY id")
-    
-    rows = cur.fetchall()
-    col_names = [desc[0] for desc in cur.description]
-    print(col_names)
+def main():
+    p = Product()
+    try:
+        conn = psycopg2.connect("dbname='investment' user='pi' host='192.168.178.54' password='ueber500mal'")
+        print('connected to database')
 
-    for row in rows:
-        print(f"{row[0]} {row[1]} {row[2]}")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM products ORDER BY id")
+        
+        rows = cur.fetchall()
+        col_names = [desc[0] for desc in cur.description]
 
-except psycopg2.DatabaseError as e:
-    print (f'Error {e}')
-    sys.exit(1)
+        for row in rows:
+            p.wkn = row[1]
+            p.name = row[4]
+            print(f'{p.wkn} {p.name}')
 
-finally:
-    if conn:
-        conn.close()
+    except psycopg2.DatabaseError as e:
+        print (f'Error {e}')
+        sys.exit(1)
+
+    finally:
+        if conn:
+            conn.close()
+
+if __name__ == "__main__":
+    main()
