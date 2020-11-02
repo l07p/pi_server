b5393499e04d8babaf03efab45cd9dfabb4bb080
project <project@ubuntu.fritz.box>
2020-11-02 18:40:30 +0000

    access database tryed in product.py

--- database/investment/scripts/python/product.py
+++ database/investment/scripts/python/product.py
@@ -2,8 +2,27 @@
 # This class treats with product and its table in database 'investment'
 
 import psycopg2
+import psycopg2.extras
+import sys
 
 try:
-    conn = psycopg.connect("dbname='investment' user='pi' host='localhost' password='ueber500mal'")
-except:
-    print ('I am unable to connect to the database')
+    conn = psycopg2.connect("dbname='investment' user='pi' host='192.168.178.54' password='ueber500mal'")
+    print('connected to database')
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM products ORDER BY id")
+    
+    rows = cur.fetchall()
+    col_names = [desc[0] for desc in cur.description]
+    print(col_names)
+
+    for row in rows:
+        print(f"{row[0]} {row[1]} {row[2]}")
+
+except psycopg2.DatabaseError as e:
+    print (f'Error {e}')
+    sys.exit(1)
+
+finally:
+    if conn:
+        conn.close()
