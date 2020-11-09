6824b0c8b0e15b764850402e98d21ecaee068735
l07p <l07p@gmx.de>
2020-11-09 21:53:31 +0100

    added insert product to table products

--- database/investment/scripts/python/order.py
+++ database/investment/scripts/python/order.py
@@ -12,7 +12,6 @@ class order:
         return 
     
     def orderday(self, day_str):
-        print(day_str)
         try:
             self.date = datetime.datetime.strptime(day_str, "%Y-%m-%d")
         except ValueError:
@@ -28,46 +27,30 @@ class order:
     def set_order_kurs(self, _kurs):
         self.kurs = float(_kurs)
 
-
-def main(_text, _orderday, _stueck, _kurs):
-    print(_text)
+def main(_orderday, _stueck, _kurs):
     o1 = order()
     o1.orderday(_orderday)
     o1.set_order_stueck(_stueck)
     o1.set_order_kurs(_kurs)
     print(o1.build_order())
-
-        
-def valid_datetime_type(arg_datetime_str):
-    """custom argparse type for user datetime values given from the command line"""
-    try:
-        return datetime.datetime.strptime(arg_datetime_str, "%Y-%m-%d %H:%M")
-    except ValueError:
-        msg = "Given Datetime ({0}) not valid! Expected format, 'YYYY-MM-DD HH:mm'!".format(arg_datetime_str)
-        raise argparse.ArgumentTypeError(msg)  
-
+    
 if __name__ == '__main__':
     import argparse
     
     parser = argparse.ArgumentParser(description='give text')
-    
-    parser.add_argument('-si', '--string_input' ,
-                        dest='input_text',
-                        help='input text to be worked out', 
-                        default='you forgot inputting') 
     
     parser.add_argument('--orderday',
                         help='input date of the order',
                         default='2020-10-3')
     
     parser.add_argument('--stueck',
-                        help='input date of the order',
+                        help='input stueck of the order',
                         default=4.001)
     
     parser.add_argument('--kurs',
-                        help='input date of the order',
+                        help='input kurs of the order',
                         default=122.3)
     
     args = parser.parse_args()
 
-    main(_text=args.input_text, _orderday=args.orderday, _stueck=args.stueck, _kurs=args.kurs)  
+    main(_orderday=args.orderday, _stueck=args.stueck, _kurs=args.kurs)  
--- database/investment/scripts/python/product.py
+++ database/investment/scripts/python/product.py
@@ -8,31 +8,91 @@ import sys
 class Product:
     def __init__(self):
         pass
+    
+    def list_products(self):
+        try:
+            conn = psycopg2.connect("dbname='investment' user='pi' host='192.168.178.54' password='ueber500mal'")
+            print('connected to database')
 
-def main():
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM products ORDER BY id")
+            
+            rows = cur.fetchall()
+            col_names = [desc[0] for desc in cur.description]
+
+            for row in rows:
+                self.wkn = row[1]
+                self.name = row[4]
+                print(f'{self.wkn} {self.name}')
+
+        except psycopg2.DatabaseError as e:
+            print (f'Error {e}')
+            sys.exit(1)
+
+        finally:
+            if conn:
+                conn.close()        
+
+    def insert_product(self, _wkn, _isin, _name, _google_symbol):
+        """ insert a new into the products table """
+        sql = """INSERT INTO products(wkn, isin, google_symbol, name)
+                VALUES(%s, %s, %s, %s) RETURNING id;"""
+        conn = None
+        product_id = None
+        try:
+            # connect to the PostgreSQL database
+            conn = psycopg2.connect("dbname='investment' user='pi' host='192.168.178.54' password='ueber500mal'")
+            # create a new cursor
+            cur = conn.cursor()
+            # execute the INSERT statement
+            cur.execute(sql, (_wkn, _isin, _name, _google_symbol))
+            # get the generated id back
+            product_id = cur.fetchone()[0]
+            # commit the changes to the database
+            conn.commit()
+            # close communication with the database
+            cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+
+        return product_id
+
+def main(_readonly, _wkn, _isin, _name, _google_symbol):
+
     p = Product()
-    try:
-        conn = psycopg2.connect("dbname='investment' user='pi' host='192.168.178.54' password='ueber500mal'")
-        print('connected to database')
+    if _readonly:
+        p.list_products()
+    else:
+        print(p.insert_product(_wkn, _isin, _name, _google_symbol))
 
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM products ORDER BY id")
-        
-        rows = cur.fetchall()
-        col_names = [desc[0] for desc in cur.description]
 
-        for row in rows:
-            p.wkn = row[1]
-            p.name = row[4]
-            print(f'{p.wkn} {p.name}')
-
-    except psycopg2.DatabaseError as e:
-        print (f'Error {e}')
-        sys.exit(1)
-
-    finally:
-        if conn:
-            conn.close()
 
 if __name__ == "__main__":
-    main()
+    import argparse
+    
+    parser = argparse.ArgumentParser(description='insert, update or list only the product datasets')
+    
+    parser.add_argument('--readonly', default=False, action='store_true')
+    
+    parser.add_argument('--wkn',
+                        help='input wkn of the product',
+                        default='A0Q4R2')
+    
+    parser.add_argument('--isin',
+                        help='input isin of the product',
+                        default='DE000A0Q4R28')
+    
+    parser.add_argument('--name',
+                        help='input name of the product',
+                        default='iShares STOXX Europe 600 Automobiles & Parts UCITS ETF (DE)')
+        
+    parser.add_argument('--google_symbol',
+                        help='input google_symbol of the product',
+                        default='EXV5')
+    
+    args = parser.parse_args()
+
+    main(_readonly=args.readonly, _wkn=args.wkn, _isin=args.isin, _name=args.name, _google_symbol=args.google_symbol)
