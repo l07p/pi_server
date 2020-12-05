9d6d72761309d17072958d776b162497c02be566
l07p <l07p@gmx.de>
2020-12-05 20:11:02 +0100

    database?

--- database/investment/scripts/python/read_csv.py
+++ database/investment/scripts/python/read_csv.py
@@ -1,6 +1,16 @@
 import pandas as pd
 from collections import OrderedDict, defaultdict
 
+class Read_csv:
+    def __init__(self, _filepath):
+        self.filepath = _filepath 
+
+    def myfunc(self):
+        print(r'{}'. format(self.filepath))
+
+f1 = Read_csv(r"C:\Users\saver\Downloads\502081722b.csv")
+f1.myfunc()
+
 df = pd.read_csv(r"C:\Users\saver\Downloads\502081722b.csv", encoding="ISO-8859-1", low_memory=False, delimiter=';')
 df.drop(df.columns[df.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
 
