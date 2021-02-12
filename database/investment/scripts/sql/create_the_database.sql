4b1b07a484ab4ff4b45e277a503e49d4fd9931b7
l07p <l07p@gmx.de>
2021-02-12 17:55:35 +0100

    added sql script of create the database

---
+++ database/investment/scripts/sql/create_the_database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE investment
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C.UTF-8'
+    LC_CTYPE = 'C.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
